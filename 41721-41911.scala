c707d3330886753349e898a8b79567d190b57781
joaocampinhos <joao.campinhos@gmail.com>
2014-10-07 22:23:08 +0100

    If winning isn't everything, why do they keep score?

--- 41721-41911.scala
+++ 41721-41911.scala
@@ -7,12 +7,9 @@ object Decimal {
   val third: Real = Stream.cons(3, third)
   val one: Real = Stream.cons(9, one)
 
-  // Não sei se tou a gerar bem porque o noise é estranho.
-  // TODO: remover primeiro digito
   def fromDouble(d: Double): Real =
     Stream.cons((d*10).toInt, fromDouble((d*10-(d*10).toInt)))
 
-  // TODO: remover primeiro digito
   def fromFraction(num: Int, den: Int): Real =
     Stream.cons(num*10/den, fromFraction((num*10%den), den))
 
@@ -21,7 +18,8 @@ object Decimal {
   //val MAXPER = 1000
   //def per(r: Real): Int
 
-  def toDouble(r: Real): Double = r.head.toDouble/10+toDouble(r.take(15).tail)
+  def toDouble(r: Real): Double =
+    if (r.isEmpty) 0 else r.head.toDouble/10+toDouble(r.tail.take(15))/10
 
   val ints: Real = Stream.from(1)
 
@@ -41,12 +39,14 @@ object Decimal {
     //
 
     //e
-    //val x = toDouble(Stream.cons(1,Stream.cons(3,zero)))
+    val a: Real = Stream.cons(1, Stream.cons(5, Stream.cons(3,zero)))
+    val x = toDouble(third)
 
     //f
     //ints take 10 print
 
-    x take 10 print
+    //x take 10 print
+    println(x)
 
   }
 }
